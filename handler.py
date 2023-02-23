e1ae72c89ac6b8636c4c1bcfd7b5f7acd1ae917e
ZARIFMAC <uzbzarif@gmail.com>
2023-02-23 10:50:46 +0500

    Send image data to server

--- handler.py
+++ handler.py
@@ -2,6 +2,7 @@ import telegram
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters,CallbackContext
 from telegram import Update
 import logging
+import requests
 # Handler for /start command
 def start(update:Update, context:CallbackContext):
 
@@ -17,5 +18,18 @@ def get_image(update:Update, context:CallbackContext):
     # Get image id
     image_id = update.message.photo[-1].file_id
     # Print message id and image id to the log
+
     print(f"Message id: {message_id}, Image id: {image_id}")
+    # Send image to backend
+    # endpoint url
+    url = 'http://motof.pythonanywhere.com/api/addImage'
+    # Payload
+    payload = {
+        'message_id': message_id,
+        'image_id': image_id
+    }
+    # Send request
+    response = requests.post(url, json=payload)
+    # Print status code
+    print(response.status_code)
     update.message.reply_text('Got image')
